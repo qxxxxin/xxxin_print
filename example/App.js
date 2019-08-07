13fdcfd27cb19d3c800b93c786fdc20c03f1f4d8
qxxxxin <qxxxxin@163.com>
2019-08-07 11:29:01 +0800

    fix bug

--- example/App.js
+++ example/App.js
@@ -9,30 +9,122 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  NativeModules
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import HoneyPrint from'react-native-authxxxxin-print';
+
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor() {
+		super();
+
+		this.state = {
+			userText1: 'testing',
+			userText2: 'hello',
+			userText3: 'word',
+			userPrintCount: 1
+		};
+
+  }
+
+  async connectBlue(){
+    HoneyPrint.connectBlue('84:25:3F:4D:95:CF');
+  }
+
+  async printText(){
+    // row :高 col 宽
+    //(String text,String fontID,Int row,Int col)
+    HoneyPrint.printText('aaa','50',20,50);
+    HoneyPrint.printText('bbb','50',40,50);
+    HoneyPrint.printText('梨花风起正清明，游子寻春半出城。','50',80,50);
+
+    HoneyPrint.printLabelOne();
+    HoneyPrint.clearDPL();
+    console.log('打印text')
+  }
+
+
+  async qrCode(){
+    // HoneyPrint.qrCode('这是一首简单的小情歌','00',100,100);
+    HoneyPrint.qrCode('aaabbbbccc','00',100,100);
+    
+    HoneyPrint.printLabelOne(); 
+    HoneyPrint.clearDPL();
+    console.log('打印qrCode')
+  }
+
+  async printLine(){
+    HoneyPrint.printLine(40,80,40,100);
+
+    HoneyPrint.printLabelOne(); 
+    HoneyPrint.clearDPL();
+    console.log('打印printLine')
+  }
+
+  printImage(){
+    HoneyPrint.printImage('http://127.0.0.1:8989/icon.png',20,20);
+    HoneyPrint.printLabelOne(); 
+  }
+
+  //
+  closeBlue(){
+    HoneyPrint.closeBlue();
+  }
+
+  //
+  clearDPL(){
+    HoneyPrint.clearDPL();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+
+        <View>
+          <TouchableOpacity
+          style={{borderWidth:1, borderColor:'grey', padding:3, width:100, backgroundColor:'white'}}
+          onPress={ this.connectBlue}>
+            <Text>honey连接蓝牙!</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+          style={{borderWidth:1, borderColor:'grey', padding:3, width:100, backgroundColor:'white'}}
+          onPress={ this.printText}>
+            <Text>honey打印text!</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+          style={{borderWidth:1, borderColor:'grey', padding:3, width:100, backgroundColor:'white'}}
+          onPress={ this.qrCode}>
+            <Text>honey打印qrCode!</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+          style={{borderWidth:1, borderColor:'grey', padding:3, width:100, backgroundColor:'white'}}
+          onPress={ this.printLine}>
+            <Text>honey打印printLine!</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <TouchableOpacity
+          style={{borderWidth:1, borderColor:'grey', padding:3, width:100, backgroundColor:'white'}}
+          onPress={ this.closeBlue}>
+            <Text>honey打印关闭蓝牙连接!</Text>
+          </TouchableOpacity>
+        </View>
+        
       </View>
     );
   }
